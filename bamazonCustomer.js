cf91b38bee4cb952067e2aa79732153c42227629
Pickle Rick <dmryan711@gmail.com>
2019-04-01 03:30:19 -0400

    V1 Complete_removed comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -143,36 +143,3 @@ function purchaseAmount(itemId,amountInt){
       });
 }
 
-
-
-// The app should then prompt users with two messages.
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-
-
-
-
-//function displayDBResult(dbResult){
-    //     console.log(chalk.red("+-------------------------------------------------------------+"))
-    //     console.log("|\tID\t|\tPRODUCT NAME\t|\tDEPATMENT NAME\t|\tPRICE\t|\tQUANTITY\t|");
-    //     dbResult.forEach(element => {
-    //         console.log(chalk.red("+-------------------------------------------------------------+"))
-    //         console.log(chalk.green("\t"+element.item_id + "\t\t"+element.product_name+ "\t\t\t"+ element.department_name+"\t\t"+element.price+"\t\t"+element.stock_quantity));
-            
-    //     });
-       
-    // }
